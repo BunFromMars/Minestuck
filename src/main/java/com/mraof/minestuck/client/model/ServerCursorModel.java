64f943b70c1a7a2e1f0b0e14f99a2f7348541f30
BunFromMars <marissa.l.private@gmail.com>
2020-09-19 18:33:30 -0700

    Added Sburb Cursor Model

---
+++ src/main/java/com/mraof/minestuck/client/model/ServerCursorModel.java
@@ -0,0 +1,51 @@
+// Made with Blockbench 3.6.6
+// Exported for Minecraft version 1.15
+// Paste this class into your mod and generate all required imports
+
+
+public class ServerCursorModel extends EntityModel<Entity> {
+	private final ModelRenderer cursor;
+
+	public ServerCursorModel() {
+		textureWidth = 16;
+		textureHeight = 16;
+
+		cursor = new ModelRenderer(this);
+		cursor.setRotationPoint(11.0F, 24.0F, -12.0F);
+		cursor.setTextureOffset(0, 5).addBox(-8.0F, -4.0F, 14.0F, 8.0F, 4.0F, 8.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 8).addBox(-8.0F, -4.0F, 5.0F, 8.0F, 4.0F, 8.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 0).addBox(-17.0F, -4.0F, 5.0F, 8.0F, 4.0F, 8.0F, 0.0F, false);
+		cursor.setTextureOffset(6, 10).addBox(-13.0F, -4.0F, 14.0F, 4.0F, 4.0F, 4.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 9).addBox(-14.0F, -4.0F, 19.0F, 4.0F, 4.0F, 4.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 5).addBox(-18.0F, -4.0F, 15.0F, 4.0F, 4.0F, 8.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 1).addBox(-19.0F, -4.0F, 2.0F, 1.0F, 4.0F, 12.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 1).addBox(-20.0F, -4.0F, 14.0F, 1.0F, 4.0F, 12.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 3).addBox(-21.0F, -4.0F, 14.0F, 1.0F, 4.0F, 9.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 0).addBox(-22.0F, -4.0F, 14.0F, 1.0F, 4.0F, 7.0F, 0.0F, false);
+		cursor.setTextureOffset(1, 7).addBox(-23.0F, -4.0F, 14.0F, 1.0F, 4.0F, 5.0F, 0.0F, false);
+		cursor.setTextureOffset(2, 7).addBox(-24.0F, -4.0F, 14.0F, 1.0F, 4.0F, 3.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 11).addBox(-25.0F, -4.0F, 14.0F, 1.0F, 4.0F, 1.0F, 0.0F, false);
+		cursor.setTextureOffset(1, 2).addBox(-20.0F, -4.0F, 4.0F, 1.0F, 4.0F, 3.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 8).addBox(-23.0F, -4.0F, 11.0F, 1.0F, 4.0F, 3.0F, 0.0F, false);
+		cursor.setTextureOffset(1, 7).addBox(-22.0F, -4.0F, 11.0F, 3.0F, 4.0F, 3.0F, 0.0F, false);
+		cursor.setTextureOffset(1, 7).addBox(-21.0F, -4.0F, 6.0F, 1.0F, 4.0F, 1.0F, 0.0F, false);
+		cursor.setTextureOffset(1, 10).addBox(-24.0F, -4.0F, 13.0F, 1.0F, 4.0F, 1.0F, 0.0F, false);
+		cursor.setTextureOffset(0, 3).addBox(-24.0F, -4.0F, 7.0F, 5.0F, 4.0F, 4.0F, 0.0F, false);
+	}
+
+	@Override
+	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
+		//previously the render function, render code was moved to a method below
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		cursor.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}

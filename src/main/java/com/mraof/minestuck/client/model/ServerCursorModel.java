f8253660a629b9fe81d3d58607db9fc4e304a668
BunFromMars <marissa.l.private@gmail.com>
2020-09-24 22:52:08 -0700

    Animated Entities changes

--- src/main/java/com/mraof/minestuck/client/model/ServerCursorModel.java
+++ src/main/java/com/mraof/minestuck/client/model/ServerCursorModel.java
@@ -2,13 +2,15 @@
 // Exported for Minecraft version 1.12.2 or 1.15.2 (same format for both) for entity models animated with GeckoLib
 // Paste this class into your mod and follow the documentation for GeckoLib to use animations. You can find the documentation here: https://github.com/bernie-g/geckolib
 // Blockbench plugin created by Gecko
-package com.example.mod;
+package com.mraof.minestuck.client.model;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib.animation.model.AnimatedEntityModel;
 import software.bernie.geckolib.animation.render.AnimatedModelRenderer;
+import software.bernie.geckolib.entity.IAnimatedEntity;
 
-public class ServerCursorModel extends AnimatedEntityModel<Entity> {
+public class ServerCursorModel extends AnimatedEntityModel<IAnimatedEntity> {
 
     private final AnimatedModelRenderer cursor;
 	private final AnimatedModelRenderer roof1;
